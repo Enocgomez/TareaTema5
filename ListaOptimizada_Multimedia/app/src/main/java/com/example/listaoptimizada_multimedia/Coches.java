3aca22735986092ae29cf0aa591888c2ca216b50
Enocgomez <egc0031@alu.medac.es>
2025-11-12 11:25:39 +0100

    Creacion de clase coches

---
+++ ListaOptimizada_Multimedia/app/src/main/java/com/example/listaoptimizada_multimedia/Coches.java
@@ -0,0 +1,16 @@
+package com.example.listaoptimizada_multimedia;
+
+public class Coches {
+    private String marca;
+
+    private Integer potencia;
+    private String imagen;
+
+    public Coches(String marca, Integer potencia, String imagen){
+        this.marca = marca;
+        this.potencia = potencia;
+        this.imagen = imagen;
+        
+    }
+
+}
